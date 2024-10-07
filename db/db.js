fbaa4ddbeeeda1fbc62e5e4d03b6b59df102d8d7
agsry <agungkdp208@gmail.com>
2024-10-07 18:19:06 +0700

    env

--- db/db.js
+++ db/db.js
@@ -1,21 +1,24 @@
-// db.js
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+// Memuat variabel dari file .env
+dotenv.config();
 
 // Membuat koneksi ke database
 const connection = mysql.createConnection({
-    host: 'localhost',       // Ganti dengan host database Anda
-    user: 'root',   // Ganti dengan username database Anda
-    password: 'BismillahKaya123', // Ganti dengan password database Anda
-    database: 'katakitamah'     // Nama database yang telah Anda buat
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Koneksi ke database
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL database:', err.stack);
-        return;
-    }
-    console.log('Connected to MySQL database as id ' + connection.threadId);
+  if (err) {
+    console.error("Error connecting to MySQL database:", err.stack);
+    return;
+  }
+  console.log("Connected to MySQL database as id " + connection.threadId);
 });
 
 // Ekspor koneksi untuk digunakan di file lain
