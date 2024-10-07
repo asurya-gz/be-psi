f7dfa223a70ca3526b8348e24e7cf73ba0a2e2f7
agsry <agungkdp208@gmail.com>
2024-10-07 18:07:42 +0700

    bakakak

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 // models/userModel.js
 const db = require("../db/db"); // Mengimpor koneksi ke database
-const bcrypt = require("bcrypt"); // Untuk hashing password
+const bcryptjs = require("bcryptjs"); // Untuk hashing password
 const jwt = require("jsonwebtoken"); // Import jsonwebtoken
 
 // Mendapatkan semua pengguna
@@ -12,7 +12,7 @@ const getAllUsers = (callback) => {
 const addUser = (userData, callback) => {
   const { username, password, email } = userData; // Hapus notification_interval
   // Hash password sebelum menyimpan ke database
-  bcrypt.hash(password, 10, (err, hashedPassword) => {
+  bcryptjs.hash(password, 10, (err, hashedPassword) => {
     if (err) {
       return callback(err);
     }
@@ -34,18 +34,16 @@ const loginUser = (email, password, callback) => {
 
     const user = results[0];
 
-    bcrypt.compare(password, user.password, (err, isMatch) => {
+    bcryptjs.compare(password, user.password, (err, isMatch) => {
       if (err) {
         return callback(err);
       }
 
       if (isMatch) {
         // Buat token JWT
-        const token = jwt.sign(
-          { id: user.id, email: user.email },
-          "user123",
-          { expiresIn: "7d" }
-        );
+        const token = jwt.sign({ id: user.id, email: user.email }, "user123", {
+          expiresIn: "7d",
+        });
         return callback(null, { user, token }); // Kembalikan user dan token
       } else {
         return callback(null, false);
@@ -55,14 +53,14 @@ const loginUser = (email, password, callback) => {
 };
 
 const changePassword = (userId, newPassword, callback) => {
-    // Hash password baru sebelum menyimpannya di database
-    bcrypt.hash(newPassword, 10, (err, hashedPassword) => {
-      if (err) {
-        return callback(err);
-      }
-      const query = "UPDATE users SET password = ? WHERE id = ?";
-      db.query(query, [hashedPassword, userId], callback);
-    });
-  };
-  
-  module.exports = { getAllUsers, addUser, loginUser, changePassword };
+  // Hash password baru sebelum menyimpannya di database
+  bcryptjs.hash(newPassword, 10, (err, hashedPassword) => {
+    if (err) {
+      return callback(err);
+    }
+    const query = "UPDATE users SET password = ? WHERE id = ?";
+    db.query(query, [hashedPassword, userId], callback);
+  });
+};
+
+module.exports = { getAllUsers, addUser, loginUser, changePassword };
